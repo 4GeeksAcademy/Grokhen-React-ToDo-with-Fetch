c03158d2389cf6391a7bbaa325f9ce0b399ce776
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-03-03 10:38:54 +0000

    function PUT added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,57 +12,57 @@ const Home = () => {
     }
     const sendData = (e) => {
         e.preventDefault();
-        const newTodo = {id: key, label: input, done: false};
-        setToDo([...toDo, newTodo]);
-        setKey(key +1);
+        const newTodo = { id: key, label: input, done: false };
+        const updateList = [...toDo, newTodo];
+        setToDo(updateList);
+        updateToDoApi(updateList);
+        setKey(key + 1);
         setInput("");
     }
 
-    const handleRemove = (id) =>{
+    const handleRemove = (id) => {
         const updatedToDo = toDo.filter((task) => task.id !== id);
-    setToDo(updatedToDo);
+        setToDo(updatedToDo);
+        updateToDoApi(updatedToDo)
     }
 
     const fetchToDo = async (endpoint, config) => {
         const response = await fetch(endpoint);
         const toDoData = await response.json();
         setToDo(toDoData);
-        
+
     }
 
     useEffect(() => {
         fetchToDo("https://playground.4geeks.com/apis/fake/todos/user/grokhen", { method: "GET" })
     }, [])
 
-    const fetchPutToDo = async (endpoint, config) => {
+    const updateToDoApi = async (upDateData) => {
         try {
-            const response = await fetch(endpoint, {
+            const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/grokhen", {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(toDo)
+                body: JSON.stringify(upDateData)
             });
             if (!response.ok) {
                 throw new Error('Error al enviar solicitud PUT');
             }
             const newData = await response.json();
-            setToDo(newData.map(task => task.label));
+            console.log(newData);
         } catch (error) {
             console.error('Error al enviar solicitud PUT:', error);
         }
     }
 
-    useEffect(() => {
-        fetchPutToDo("https://playground.4geeks.com/apis/fake/todos/user/grokhen", { method: "PUT" })
-    }, [toDo])
 
     return (
         <>
-            <h1>Tareas pendiente</h1>
+            <h1>Tareas pendientes</h1>
             <AddToDo input={input} newTask={newTask} sendData={sendData} />
             <ul>
-                <ToDo toDo={toDo} handleRemove={handleRemove}/>
+                <ToDo toDo={toDo} handleRemove={handleRemove} />
             </ul>
         </>
     )
